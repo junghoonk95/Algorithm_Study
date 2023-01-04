e71f040232c7c7b8d632e6cb0ea1726d575bcdb1
DODO <pch145@naver.com>
2023-01-04 17:31:31 +0900

    Create 구명보트_강두원.py

---
+++ 2주차/2)구명보트/구명보트_강두원.py
@@ -0,0 +1,29 @@
+'''
+Concept : two-pointer, greedy
+
+greedy 방식으로 현재 가장 무거운 사람과 가장 가벼운 사람이 같이 보트에 탈 수 없을 경우 
+그 사람은 무조건 혼자 타야함
+'''
+
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+    lPtr, rPtr = 0, len(people)-1
+    
+    w = 0
+    while lPtr <= rPtr:            
+        if people[rPtr] + w <= limit:
+            w += people[rPtr]
+            rPtr -= 1
+        elif people[lPtr] + w <= limit:
+            w += people[lPtr]
+            lPtr += 1
+        else:
+            answer += 1
+            w = 0
+    
+    if w > 0:
+        answer += 1
+    
+    return answer
