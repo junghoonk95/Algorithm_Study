eefb77002517b9b794adb1ececc79aace3c86e2b
DODO <pch145@naver.com>
2023-01-05 09:13:09 +0900

    Update 평범한 배낭_강두원.cpp

--- 2주차/5)평범한 배낭/평범한 배낭_강두원.cpp
+++ 2주차/5)평범한 배낭/평범한 배낭_강두원.cpp
@@ -20,18 +20,12 @@ int main() {
 
     cin >> N >> K;
 
-    for(int i = 1; i <= N; ++i) {
-        cin >> w[i] >> v[i];
-    }
+    for(int i = 1; i <= N; ++i) cin >> w[i] >> v[i];
 
     for(int i = 1; i <= N; ++i) {
         for(int j = 1; j <= K; ++j) {
-            if (j >= w[i]) {
-                dp[i][j] = MAX(dp[i-1][j], v[i] + dp[i-1][j-w[i]]);
-            }
-            else {
-                dp[i][j] = dp[i-1][j];
-            }
+            if (j >= w[i]) dp[i][j] = MAX(dp[i-1][j], v[i] + dp[i-1][j-w[i]]);
+            else dp[i][j] = dp[i-1][j];
         }
     }
     cout << dp[N][K] << '\n';
