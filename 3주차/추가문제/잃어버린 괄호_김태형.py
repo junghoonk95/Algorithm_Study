43e708f9c1571eb91b41d0c881bf5ff97dde807e
taehyeong51 <107748183+taehyeong51@users.noreply.github.com>
2023-01-11 16:09:47 +0900

    김태형 문제풀이

---
+++ 3주차/추가문제/잃어버린 괄호_김태형.py
@@ -0,0 +1,21 @@
+equation = list(input())
+is_processing = False
+for i in range(len(equation)):
+    if is_processing:
+        if equation[i] == '-':
+            equation[i] = ','
+            # is_processing = False
+        # elif i == len(equation)-1:
+        #     equation[i] = equation[i] + ')'
+    else:
+        if equation[i] == '-':
+            equation[i] = ','
+            is_processing = True
+
+equation = "".join(equation).split(",")
+# print(equation)
+answer = sum(list(map(int,equation[0].split("+"))))
+for numbers in equation[1:]:
+    answer -= sum(list(map(int,numbers.split("+"))))
+
+print(answer)
