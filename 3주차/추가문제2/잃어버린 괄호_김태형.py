42e80e8925ae76f6e49b167176e20343aebd5c55
taehyeong51 <107748183+taehyeong51@users.noreply.github.com>
2023-01-12 17:25:18 +0900

    Update 잃어버린 괄호_김태형.py

--- 3주차/추가문제2/잃어버린 괄호_김태형.py
+++ 3주차/추가문제2/잃어버린 괄호_김태형.py
@@ -4,10 +4,6 @@ for i in range(len(equation)):
     # if is_processing:
     if equation[i] == '-':
         equation[i] = ','
-    else:
-        if equation[i] == '-':
-            equation[i] = ','
-        # is_processing = True
 
 equation = "".join(equation).split(",")
 answer = sum(list(map(int,equation[0].split("+"))))
