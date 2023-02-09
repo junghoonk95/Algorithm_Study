e175cb98799325a5056accdfa34ddc4bbba28289
DODO <pch145@naver.com>
2023-02-09 21:39:08 +0900

    Update 수 정렬하기 2_강두원.cpp

--- 7주차/4)수 정렬하기 2/수 정렬하기 2_강두원.cpp
+++ 7주차/4)수 정렬하기 2/수 정렬하기 2_강두원.cpp
@@ -7,35 +7,31 @@ int tb[MAX];
 int temp_tb[MAX];
 
 void merge_sort(int s, int e) {
-    int m = (s+e)/2;
-
     if(s >= e) return;
 
+    // divide step //
+    int m = (s+e)/2;
     merge_sort(s, m);
     merge_sort(m+1, e);
-
+ 
+    // conquer & merge step //
     int l = s, r = m+1;
-
-    int cur = 0;
-    while (cur < e - s + 1) {
+    for(int i = 0; i < e-s+1; ++i){
+        // compare 2 arrays
         if(l <= m  && r <= e) {
-            if(tb[l] > tb[r]) {
-                temp_tb[cur++] = tb[r++];
-            }
-            else {
-                temp_tb[cur++] = tb[l++];
-            }
+            // add a smaller one to temp
+            if(tb[l] > tb[r]) temp_tb[i] = tb[r++];
+            else temp_tb[i] = tb[l++];
         }
+        // append the rest array to temp
         else {
-            if(l <= m) {
-                temp_tb[cur++] = tb[l++];
-            }
-            else if(r <= e) {
-                temp_tb[cur++] = tb[r++];
-            }
+            if(r <= e) temp_tb[i] = tb[r++];
+            else if(l <= m) temp_tb[i] = tb[l++];
         }
     }
-    copy(temp_tb, temp_tb + cur, tb + s);
+
+    // apply temp to tb
+    copy(temp_tb, temp_tb + e-s+1, tb + s);
 }
 
 int main() {
@@ -43,12 +39,9 @@ int main() {
 
     cin >> N;
 
-    for(int i = 0; i < N; ++i) {
-        cin >> tb[i];
-    }
+    for(int i = 0; i < N; ++i) cin >> tb[i];
+
     merge_sort(0, N-1);
 
-    for(int i = 0; i < N; ++i) {
-        cout << tb[i] << "\n";
-    }
+    for(int i = 0; i < N; ++i) cout << tb[i] << "\n";
 }
